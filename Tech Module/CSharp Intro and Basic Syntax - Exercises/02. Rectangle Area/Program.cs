9b3c85137f6e43cfa19e16d3c57f05559abe7f32
Atanas.Dimov <nasko312@gmail.com>
2017-09-27 21:03:06 +0300

    Upload first two lecture

---
+++ Tech Module/CSharp Intro and Basic Syntax - Exercises/02. Rectangle Area/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02._Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideOne = double.Parse(Console.ReadLine());
+            double sideTwo = double.Parse(Console.ReadLine());
+
+            double sum = sideOne * sideTwo;
+
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
---
+++ Tech Module/CSharp Intro and Basic Syntax - Exercises/04. Beverage Labels/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContent = int.Parse(Console.ReadLine());
+            int sugarContent = int.Parse(Console.ReadLine());
+
+            double energy = (volume * energyContent) / 100.00;
+            double sugar = (sugarContent * volume) / 100.00;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energy}kcal, {sugar}g sugars");
+        }
+    }
+}
